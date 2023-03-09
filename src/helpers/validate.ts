7dbb9ba784d5eaf1869f93c6c16774132959ee9f
Emilio Sel <aimiliossel@hotmail.gr>
2023-03-09 23:43:07 +0200

    Generic validation function

---
+++ src/helpers/validate.ts
@@ -0,0 +1,21 @@
+import { plainToInstance } from "class-transformer";
+import { validate, ValidationError } from "class-validator";
+import { UnprocessableEntityError } from "errors/errors";
+
+export async function transformAndValidate<T>(input: unknown, dtoClass: new () => T): Promise<T> {
+  const dtoObject = plainToInstance(dtoClass, input);
+  const errors: ValidationError[] = await validate(dtoObject as object);
+
+  if (errors.length > 0) {
+    const readableErrors = errors.map(error => {
+      const constraints = error.constraints;
+      if (constraints) {
+        return Object.values(constraints).join("; ");
+      }
+      return error.toString();
+    });
+
+    throw new UnprocessableEntityError(readableErrors.join("\n"));
+  }
+  return dtoObject;
+}
